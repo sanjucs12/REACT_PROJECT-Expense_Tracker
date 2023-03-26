9cb928845e88acb8935cda8682d95891b1dc8496
sanjucs12 <sanjucs12@gmail.com>
2023-03-27 00:20:55 +0530

    Added Expense Item

---
+++ components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        <h2>Expense Item!</h2>
+      </div>
+      <div>Food Rs 500</div>
+      <div>Petrol Rs 300</div>
+      <div>Movies Rs 200</div>
+    </div>
+  );
+}
+export default ExpenseItem;
