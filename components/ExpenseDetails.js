5bd80ebff8bd97ee207ed084d2075af858b87d6b
sanjucs12 <sanjucs12@gmail.com>
2023-03-27 17:16:19 +0530

    Splitting the Components

---
+++ components/ExpenseDetails.js
@@ -0,0 +1,9 @@
+function ExpenseDetails(props){
+    return(
+    <div className="expense-item__description">
+    <div className="expense-item__place">{props.place}</div>
+    <h2>{props.title}</h2>
+    <div className="expense-item__price">{props.price}</div>
+  </div>)
+}
+export default ExpenseDetails;
--- components/ExpenseItem.js
+++ components/ExpenseItem.js
@@ -1,17 +1,16 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 
 function ExpenseItem(props) {
-
   return (
     <div className="expense-item">
-      <div>
-        <h2>{props.date.toISOString()}</h2>
-      </div>
-      <div className="expense-item__description">
-      <div className="expense-item__place">{props.place}</div>
-        <h3>{props.title}</h3>        
-        <div className="expense-item__price">{props.price}</div>        
-      </div>
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        title={props.title}
+        place={props.place}
+        price={props.price}
+      />
     </div>
   );
 }
