78ef174f7d9e15d5e5f55a7370c571e31713eed0
sanjucs12 <sanjucs12@gmail.com>
2023-04-04 00:15:51 +0530

    made year filter work

--- components/Expenses/Expenses.js
+++ components/Expenses/Expenses.js
@@ -13,13 +13,17 @@ function Expenses(props) {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.items.map((expense) => (
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
