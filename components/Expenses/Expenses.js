d04d23d41ba66152b352f5aa17a0d3bfe1cfca93
sanjucs12 <sanjucs12@gmail.com>
2023-04-04 02:22:11 +0530

    Chart Added

--- components/Expenses/Expenses.js
+++ components/Expenses/Expenses.js
@@ -5,6 +5,7 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 //import ExpenseDetails from "./ExpenseDetails";
 
 function Expenses(props) {
@@ -24,6 +25,7 @@ function Expenses(props) {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses}/>
       <ExpensesList items={filteredExpenses}/>
     </Card>
   );
