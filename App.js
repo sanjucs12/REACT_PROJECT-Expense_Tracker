b6f1a8f8da5349b14fac3ff0a13727cb1ff323b9
sanjucs12 <sanjucs12@gmail.com>
2023-04-03 14:04:12 +0530

    Form added for new entries

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 //import ExpenseItem from "./components/ExpenseItem";
 import Expenses from "./components/Expenses/Expenses";
 
+import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
   const expenses = [
     {
@@ -34,7 +35,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items={expenses} />
     </div>
   );
