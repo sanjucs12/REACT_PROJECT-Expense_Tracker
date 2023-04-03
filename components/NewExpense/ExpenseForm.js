6d49506c088619bb724ea4324355fc6249d24a8e
sanjucs12 <sanjucs12@gmail.com>
2023-04-03 15:24:50 +0530

    using one state insted of multiple states

--- components/NewExpense/ExpenseForm.js
+++ components/NewExpense/ExpenseForm.js
@@ -2,26 +2,48 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredLocation, setEnteredLocation] = useState("");
-  const [enteredAmount, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  //   const [enteredTitle, setEnteredTitle] = useState("");
+  //   const [enteredLocation, setEnteredLocation] = useState("");
+  //   const [enteredAmount, setEnteredAmount] = useState("");
+  //   const [enteredDate, setEnteredDate] = useState("");
+
+  const [userInput,setUserInput]=useState({
+    enteredTitle: "",
+    enteredLocation: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    //setEnteredTitle(event.target.value);
+    setUserInput({
+        ...userInput,
+        enteredTitle:event.target.value
+    })
   };
 
   const locationChangeHandler = (event) => {
-    setEnteredLocation(event.target.value);
+    //setEnteredLocation(event.target.value);
+    setUserInput({
+        ...userInput,
+        enteredLocation:event.target.value
+    })
   };
 
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    //setEnteredAmount(event.target.value);
+    setUserInput({
+        ...userInput,
+        enteredAmount:event.target.value
+    })
   };
 
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
+    //setEnteredDate(event.target.value);
+    setUserInput({
+        ...userInput,
+        enteredDate:event.target.value
+    })
   };
 
   return (
